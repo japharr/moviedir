ef92cd71e43385f79dd76565f859d5cb4b83fb6c
Jaleel Adesina <jtutoff@gmail.com>
2021-03-27 18:50:09 +0100

    refactored internet connectivity

--- app/src/main/java/com/jeliliadesina/moviedir/util/ConnectivityUtil.kt
+++ app/src/main/java/com/jeliliadesina/moviedir/util/ConnectivityUtil.kt
@@ -2,12 +2,34 @@ package com.jeliliadesina.moviedir.util
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
+import android.os.Build
+
 
 object ConnectivityUtil {
     fun isConnected(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
+
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+                val cap = cm.getNetworkCapabilities(cm.activeNetwork) ?: return false
+                return cap.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
+                val networks = cm.allNetworks
+                for (n in networks) {
+                    val nInfo = cm.getNetworkInfo(n)
+                    if (nInfo != null && nInfo.isConnected) return true
+                }
+            }
+            else -> {
+                val networks = cm.allNetworkInfo
+                for (nInfo in networks) {
+                    if (nInfo != null && nInfo.isConnected) return true
+                }
+            }
+        }
+
+        return false;
     }
 }
